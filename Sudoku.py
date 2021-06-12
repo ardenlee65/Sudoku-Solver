4ab86a6fc2e7f11d43030e4195252f63d7e6db7b
Arden Lee <50338472+ardenlee65@users.noreply.github.com>
2021-06-12 15:15:27 -0700

    Finished algorithm

--- Sudoku.py
+++ Sudoku.py
@@ -2,8 +2,10 @@ class Board:
     def __init__(self, inputBoard):
         self.board = inputBoard
 
-    #def __str__(self):
+    def __str__(self):
         # call print function
+        self.printBoard()
+        return ""
 
     def printBoard(self):
         rows = len(self.board)
@@ -14,10 +16,66 @@ class Board:
                 if (row != 0) and (row%3 == 0):
                     print("|", end=" ")
                 print(self.board[col][row], end=" ")
-
             print()
+        print()
 
+    def find_empty(self):
+        rows = len(self.board)
+        for row in range(rows):
+            for col in range(rows):
+                if (self.board[row][col] == 0):
+                    return (row, col)
+        
+        # Empty space cannot be found
+        return ()
 
+    def valid(self, num, x, y):
+        # num is the number we are trying, x is row, y is col
+        # need to check vertical, horizontal, and in the 3x3 square
+
+        bound = len(self.board)
+
+        # horizontal
+        for row in range(bound):
+            if (self.board[row][y] == num) and (row != x):
+                return False
+
+        # vertical
+        for col in range(bound):
+            if (self.board[x][col] == num) and (col != y):
+                return False
+
+        # 3x3
+        startRow = (x // 3)*3
+        startCol = (y // 3)*3
+        for row in range(3):
+            for col in range(3):
+                if (self.board[startRow + row][startCol + col] == num) and ((startRow + row) != x) and ((startCol + col) != y):
+                    return False
+
+        return True
+
+    def solve(self):
+        moves = self.find_empty()
+
+        # If no more moves, means we filled up the board legally
+        if not moves:
+            return True
+        else:
+            row, col = moves
+
+        # Try every num
+        for attempt in range(1, 10):
+            if self.valid(attempt, row, col):
+                self.board[row][col] = attempt
+
+                if self.solve():
+                    return True
+
+                # If we get here, it means the move we chose did not work
+                self.board[row][col] = 0
+
+        return False
 
 
 testBoard = [
@@ -33,4 +91,6 @@ testBoard = [
             ]
 
 game = Board(testBoard)
+game.printBoard()
+game.solve()
 game.printBoard()
