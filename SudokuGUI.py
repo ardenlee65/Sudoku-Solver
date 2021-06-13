9804bf8bd8a50fb51893d15d963dbb95a7207efa
Arden Lee <50338472+ardenlee65@users.noreply.github.com>
2021-06-12 19:05:29 -0700

    Getting started with GUI

--- SudokuGUI.py
+++ SudokuGUI.py
@@ -1,3 +1,5 @@
+import pygame
+
 class Board:
     def __init__(self, inputBoard):
         self.board = inputBoard
@@ -64,7 +66,7 @@ class Board:
         else:
             row, col = moves
 
-        # Try every num
+        # Try every valid num
         for attempt in range(1, 10):
             if self.valid(attempt, row, col):
                 self.board[row][col] = attempt
@@ -72,13 +74,15 @@ class Board:
                 if self.solve():
                     return True
 
-                # If we get here, it means the move we chose did not work
+                # If we get here, it means the number we chose did not work
                 self.board[row][col] = 0
 
         return False
 
 
-testBoard = [
+if __name__ == "__main__":
+    # Find answer to board
+    testBoard = [
             [0,0,0,2,6,0,7,0,1],
             [6,8,0,0,7,0,0,9,0],
             [1,9,0,0,0,4,5,0,0],
@@ -90,7 +94,19 @@ testBoard = [
             [7,0,3,0,1,8,0,0,0],
             ]
 
-game = Board(testBoard)
-game.printBoard()
-game.solve()
-game.printBoard()
+    game = Board(testBoard)
+    game.solve()
+    answerBoard = game.board
+
+    pygame.init()
+    screen = pygame.display.set_mode((450, 600))
+    icon = pygame.image.load('sudoku-svgrepo-com.svg')
+    pygame.display.set_icon(icon)
+    pygame.display.set_caption("Sudoku")
+
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
